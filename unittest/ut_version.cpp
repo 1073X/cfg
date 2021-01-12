4cd34657cb624a7dcd31e0fd4f297ce350c06758
Joe Wu <wuzongsheng@gmail.com>
2021-01-12 14:46:42 +0800

    init project

--- unittest/ut_version.cpp
+++ unittest/ut_version.cpp
@@ -2,12 +2,12 @@
 
 #include <iostream>
 
-namespace miu::project {
+namespace miu::cfg {
 extern std::string_view version();
 extern std::string_view build_info();
-}    // namespace miu::project
+}    // namespace miu::cfg
 
 TEST(ut_version, version) {
-    std::cout << miu::project::version() << std::endl;
-    std::cout << miu::project::build_info() << std::endl;
+    std::cout << miu::cfg::version() << std::endl;
+    std::cout << miu::cfg::build_info() << std::endl;
 }
