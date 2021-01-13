137c1acdac2a36c0e7a285a81117a511b23a6204
Joe Wu <wuzongsheng@gmail.com>
2021-01-13 13:44:48 +0800

    get item by name and idx

--- source/lib/json_source.hpp
+++ source/lib/json_source.hpp
@@ -2,6 +2,7 @@
 
 #include <nlohmann/json.hpp>
 
+#include "json2var.hpp"
 #include "source.hpp"
 
 namespace miu::cfg {
@@ -15,11 +16,22 @@ class json_source : public source {
     std::string_view name() const override { return _name; }
 
     // get by index
-    com::variant get(uint32_t) const { return {}; }
+    com::variant get(uint32_t idx) const {
+        if (_src.is_array() && idx < _src.size()) {
+            return json2var(_src.at(idx));
+        }
+        return {};
+    }
+
     source const* get_child(uint32_t) const override { return nullptr; }
 
-    // get by name
-    com::variant get(std::string_view) const override { return {}; }
+    com::variant get(std::string_view key) const override {
+        if (_src.is_object() && _src.contains(key.data())) {
+            return json2var(_src.at(key.data()));
+        }
+        return {};
+    }
+
     source const* get_child(std::string_view) const override { return nullptr; }
 
   private:
--- unittest/ut_json_source.cpp
+++ unittest/ut_json_source.cpp
@@ -8,3 +8,23 @@ TEST(ut_json_source, name) {
     json_source src { "name", nlohmann::json {} };
     EXPECT_EQ("name", src.name());
 }
+
+TEST(ut_json_source, get_by_name) {
+    nlohmann::json json;
+    json["item"] = 1;
+
+    json_source src { "", json };
+    EXPECT_EQ(1, src.get("item").get<int32_t>());
+
+    EXPECT_FALSE(src.get("not_exists").get<int32_t>().has_value());
+}
+
+TEST(ut_json_source, get_by_idx) {
+    nlohmann::json json;
+    json[0] = 1;
+
+    json_source src { "", json };
+    EXPECT_EQ(1, src.get(0).get<int32_t>());
+
+    EXPECT_FALSE(src.get(1).get<int32_t>().has_value());
+}
